8367249e4609f3b765092cdf50295657ade99b65
johnnymcmike <mbjmcm@gmail.com>
2021-09-21 20:52:31 -0400

    (maybe) fix drive controls as per gavins advice

--- src/main.cpp
+++ src/main.cpp
@@ -72,33 +72,54 @@ void opcontrol()
 {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	//drive motors
-	pros::Motor backLeft(11); 		//PORT 11 GOES TO BACK LEFT MOTOR
-	pros::Motor frontLeft(12); 		//PORT 12 GOES TO FRONT LEFT MOTOR
-	pros::Motor frontRight(19);		//PORT 19 GOES TO FRONT RIGHT MOTOR
-	pros::Motor backRight(20);		//PORT 20 GOES TO BACK RIGHT MOTOR
-	//arm motors
-	pros::Motor armMove(1);			//TODO: placeholder port, change to real one
+	pros::Motor backLeft(11);	//PORT 11 GOES TO BACK LEFT MOTOR
+	pros::Motor frontLeft(12);	//PORT 12 GOES TO FRONT LEFT MOTOR
+	pros::Motor frontRight(19); //PORT 19 GOES TO FRONT RIGHT MOTOR
+	pros::Motor backRight(20);	//PORT 20 GOES TO BACK RIGHT MOTOR
+	//arm
+	pros::Motor armMove(9); //TODO: placeholder port, change to real one
+	pros::Motor backArmMove(8);
+
+	armMove.set_brake_mode(MOTOR_BRAKE_HOLD);
+	backArmMove.set_brake_mode(MOTOR_BRAKE_HOLD);
 
 	while (true)
 	{
 		//get controller inputs and store them to variables
-		int leftInput = master.get_analog(ANALOG_LEFT_Y) * -1; 	//yes, its inefficient to invert twice like this
-		int rightInput = master.get_analog(ANALOG_RIGHT_Y); 	//do i care? no and neither will the compiler
+		int leftInput = master.get_analog(ANALOG_LEFT_Y) * -1; //yes, its inefficient to invert twice like this
+		int rightInput = master.get_analog(ANALOG_RIGHT_Y);	   //do i care? no and neither will the compiler
 
 		//driving controls
 		backLeft.move(leftInput);
-		frontLeft.move(leftInput * -1);			//the movement of the front motors is inverted to make all wheels go
-		frontRight.move(rightInput * -1); 		//the same direction instead of just rotating inwards
+		frontLeft.move(leftInput * -1);	  //the movement of the front motors is inverted to make all wheels go
+		frontRight.move(rightInput * -1); //the same direction instead of just rotating inwards
 		backRight.move(rightInput);
 
 		//arm movement controls
-		if(master.get_digital(DIGITAL_L1))		//if user is holding down L1,
+		if (master.get_digital(DIGITAL_L1)) 	//if user is holding down L1,
 		{
-			armMove.move(10);					//move motor +10v
+			armMove.move(127); 					//move front arm motor forward
 		}
-		else if(master.get_digital(DIGITAL_R1))	//if user is holding R1 and isnt holding L1,
+		else if (master.get_digital(DIGITAL_L2))//if user is holding L2 and isnt holding L1,
 		{
-			armMove.move(-10);					//move motor -10v
+			armMove.move(-127); 				//move front arm motor backward
+		}
+		else									//otherwise,
+		{
+			armMove.move(0);					//quit moving the arm
+		}
+
+		if (master.get_digital(DIGITAL_R1)) 	//if user is holding down R1,
+		{
+			backArmMove.move(127); 				//move back arm motor forward
+		}
+		else if (master.get_digital(DIGITAL_R2))//if user is holding R2 and isnt holding R1,
+		{
+			backArmMove.move(-127); 			//move back arm motor backward
+		}
+		else									//otherwise,
+		{
+			backArmMove.move(0);				//quit moving the arm
 		}
 
 		pros::delay(5);
