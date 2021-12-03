931bd85a50f6da674fa4e83f4f8539e6d8ddf3ad
johnnymcmike <mbjmcm@gmail.com>
2021-12-03 18:00:05 -0500

    fix autonomous, begin okapilib stuff

--- include/main.h
+++ include/main.h
@@ -14,7 +14,7 @@
 
 #ifndef _PROS_MAIN_H_
 #define _PROS_MAIN_H_
-
+// using namespace okapi;
 /**
  * If defined, some commonly used enums will have preprocessor macros which give
  * a shorter, more convenient naming pattern. If this isn't desired, simply
--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -9,44 +9,48 @@ pros::Motor frontLeft(12);	//PORT 12 GOES TO FRONT LEFT MOTOR
 pros::Motor frontRight(19); //PORT 19 GOES TO FRONT RIGHT MOTOR
 pros::Motor backRight(20);	//PORT 20 GOES TO BACK RIGHT MOTOR
 
-void autonomous()
+void autonomous() //-12 -19
 {
-
 	std::shared_ptr<ChassisController> chassis =
 		ChassisControllerBuilder()
-			.withMotors(-11, 12, -19, 20)
-			// Green gearset, 4 in wheel diam, 11.5 in wheel track
-			.withDimensions(AbstractMotor::gearset::green, {{3.25_in, 10_in}, imev5GreenTPR})
+			.withMotors(
+				{12,-11},
+				{-19,20}
+			)
+			.withMaxVelocity(100.0)
+			.withDimensions({AbstractMotor::gearset::green, (3.0/5.0)}, {{3.25_in, 10_in}, imev5GreenTPR})
 			.build();
+	chassis->moveDistance(1_m);
+	chassis->turnAngle(90_deg);
 	chassis->moveDistance(1_m);
 }
 
-void translate(int units, int voltage)
-{
-	int direction = abs(units) / units;
-	//TODO:resetDriveEncoders();
-	//TODO:gyro.reset();
+// void translate(int units, int voltage)
+// {
+// 	int direction = abs(units) / units;
+// 	//TODO:resetDriveEncoders();
+// 	//TODO:gyro.reset();
 
-	while (avgDriveEncoderValue() < abs(units))
-	{
-		/* code */
-	}
-	//brief brake
-	driveMotor(-10 * direction, -10 * direction);
-	pros::delay(50);
-	//set drive back to neutral
-	driveMotor(0, 0);
-}
+// 	while (avgDriveEncoderValue() < abs(units))
+// 	{
+// 		/* code */
+// 	}
+// 	//brief brake
+// 	driveMotor(-10 * direction, -10 * direction);
+// 	pros::delay(50);
+// 	//set drive back to neutral
+// 	driveMotor(0, 0);
+// }
 
-void driveMotor(int left, int right)
-{
-	backLeft.move(left);
-	frontLeft.move(left * -1);
-	backRight.move(right * -1);
-	backRight.move(right);
-}
+// void driveMotor(int left, int right)
+// {
+// 	backLeft.move(left);
+// 	frontLeft.move(left * -1);
+// 	backRight.move(right * -1);
+// 	backRight.move(right);
+// }
 
-double avgDriveEncoderValue()
-{
-	return 0;
-}
+// double avgDriveEncoderValue()
+// {
+// 	return 0;
+// }
