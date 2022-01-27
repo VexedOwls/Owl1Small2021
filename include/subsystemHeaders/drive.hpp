4d746b1b8d5791b201b84f03d560e3a6039ab5f7
johnnymcmike <mbjmcm@gmail.com>
2022-01-26 21:31:50 -0500

    jan 26 finish up work

--- include/subsystemHeaders/drive.hpp
+++ include/subsystemHeaders/drive.hpp
@@ -1,2 +1,3 @@
 #include "main.h"
 void translate(int units, int voltage);
+void resetDriveEncoders();
--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,26 +1,45 @@
 #include "main.h"
 
-	pros::Motor backLeft(11);	//PORT 11 GOES TO BACK LEFT MOTOR
-	pros::Motor frontLeft(12, true);	//PORT 12 GOES TO FRONT LEFT MOTOR
-	pros::Motor frontmostLeft(13);
-	pros::Motor frontmostRight(18);
-	pros::Motor frontRight(19, true); //PORT 19 GOES TO FRONT RIGHT MOTOR
-	pros::Motor backRight(20);	//PORT 20 GOES TO BACK RIGHT MOTOR
+pros::Motor backLeft(11);		 //PORT 11 GOES TO BACK LEFT MOTOR
+pros::Motor frontLeft(12, true); //PORT 12 GOES TO FRONT LEFT MOTOR
+pros::Motor frontmostLeft(13);
+pros::Motor frontmostRight(18);
+pros::Motor frontRight(19, true); //PORT 19 GOES TO FRONT RIGHT MOTOR
+pros::Motor backRight(20);		  //PORT 20 GOES TO BACK RIGHT MOTOR
 
-	//arm
-	pros::Motor armMove(9);
-	pros::Motor armMove2(2);
-	pros::Motor backArmMove(1);
-	pros::Motor backArmMove2(10);
+//arm
+pros::Motor armMove(9);
+pros::Motor armMove2(2);
+pros::Motor backArmMove(1);
+pros::Motor backArmMove2(10);
 
 void autonomous()
 {
-	
 }
 
 void translate(int units, int voltage)
 {
 	//reset motor encoders
+	resetDriveEncoders();
 	//drive forwards
+	while (frontLeft.get_position() < units)
+	{
+		//setDrive(voltage, voltage);
+		pros::delay(10);
+	}
+	//brief brake
+	//setDrive(-10,-10);
+	pros::delay(50);
+	//set the drive back to neutral
+
+}
+
+void resetDriveEncoders()
+{
 	backLeft.tare_position();
+	frontLeft.tare_position();
+	frontmostLeft.tare_position();
+	backRight.tare_position();
+	frontRight.tare_position();
+	frontmostRight.tare_position();
 }
